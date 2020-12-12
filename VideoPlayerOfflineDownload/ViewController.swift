558be5d83de8a40ba71aea9ad0755360ba2382f8
Niraj <niraj.jha@go-mmt.com>
2020-12-12 11:26:38 +0530

    Initial Commit

---
+++ VideoPlayerOfflineDownload/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayerOfflineDownload
+//
+//  Created by Niraj Kumar Jha on 12/12/20.
+//  Copyright © 2020 Niraj Kumar Jha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VideoPlayerOfflineDownloadTests/VideoPlayerOfflineDownloadTests.swift
@@ -0,0 +1,34 @@
+//
+//  VideoPlayerOfflineDownloadTests.swift
+//  VideoPlayerOfflineDownloadTests
+//
+//  Created by Niraj Kumar Jha on 12/12/20.
+//  Copyright © 2020 Niraj Kumar Jha. All rights reserved.
+//
+
+import XCTest
+@testable import VideoPlayerOfflineDownload
+
+class VideoPlayerOfflineDownloadTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
